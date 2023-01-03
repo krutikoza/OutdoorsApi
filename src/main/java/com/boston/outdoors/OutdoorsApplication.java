923a33ed7d0828da8c8daedac76eaa83cba1b9c6
Krutik Oza <krutikoza@krutiks-air.lan>
2023-01-03 13:21:59 -0500

    Initial  Backend

---
+++ src/main/java/com/boston/outdoors/OutdoorsApplication.java
@@ -0,0 +1,13 @@
+package com.boston.outdoors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutdoorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutdoorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boston/outdoors/OutdoorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boston.outdoors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdoorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
